e24fc97fd4031e7503d9a84c97e1057c1ea7cbb4
rgillespie23 <rgillespie@uchicago.edu>
2023-02-24 00:07:57 +0000

    Lot of work so far

---
+++ app/models/movie.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+
+  has_many(:characters, { :class_name => "Character", :foreign_key => "movie_id", :dependent => :destroy })
+
+  belongs_to(:director, { :required => true, :class_name => "Director", :foreign_key => "director_id" })
+
+  has_many(:actors, { :through => :characters, :source => :actor })
+
+end
